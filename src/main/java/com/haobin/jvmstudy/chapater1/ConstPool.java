d0cdfc5486059c48264a20fb8e82bca62fab5370
haobin <haobin@thecover.co>
2019-08-02 10:17:15 +0800

    反射修改常量池类的值

--- src/main/java/com/haobin/jvmstudy/chapater1/ConstPool.java
+++ src/main/java/com/haobin/jvmstudy/chapater1/ConstPool.java
@@ -1,5 +1,7 @@
 package com.haobin.jvmstudy.chapater1;
 
+import java.lang.reflect.Field;
+
 /**
  * @Author haobin
  * @Date 2019-07-29 23:18
@@ -27,17 +29,48 @@ public class ConstPool {
 //        String str1 = new String("ab" + "c");
 
 
-        String s1 = new String("1") + new String("1");
-        s1.intern();
-        String s2 = "11";
-        System.out.println(s1 == s2);
-
-        String s3 = new String("2") + new String("2");
-        String s4 = "22";
+//        String s1 = new String("1") + new String("1");
+//        s1.intern();
+//        String s2 = "11";
+//        System.out.println(s1 == s2);
+//
+//        String s3 = new String("2") + new String("2");
+//        String s4 = "22";
 //        s3.intern();
-        System.out.println(s3 == s4);
+//        System.out.println(s3 == s4);
 
 
+        /**
+         * 通过反射来修改String 常量池中的值
+         */
+        String s1 = "abcd";
+        String s2 = "abcd";
+        String s3 = new String("abcd");
+        String s4 = new String(s1);
+        System.out.println("s1==s2:" + (s1 == s2));
+        System.out.println("s1==s3:" + (s1 == s3));
+        System.out.println("s3==s4:" + (s3 == s4));
+        try {
+            Field f = String.class.getDeclaredField("value");
+            System.out.print("\nAccessible: " + f.isAccessible());
+            f.setAccessible(true);
+            System.out.println(" -> " + f.isAccessible());
+            char[] v = (char[]) f.get(s1);// 获取S1的内部value数组
+            System.out.print("\nvalue:");
+            System.out.println(v);
+            v[0] = 'x';// 修改数组
+            v = new char[1];// 目测因为是get到的是引用的复制，这个引用改变不影响原引用
+            v[0] = 'y';
+            System.out.print("value:");
+            System.out.println(v);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("\ns1:" + s1 + "\ns2:" + s2 + "\ns3:" + s3 + "\ns4:" + s4);
+        System.out.println("\ns1==s2:" + (s1 == s2));
+        System.out.println("s1==s3:" + (s1 == s3));
+        System.out.println("s3==s4:" + (s3 == s4));
 
     }
 }
